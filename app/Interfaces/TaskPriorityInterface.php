9821c200ed527000d3e129b3ed33d39b8a20e53d
AraceliHT <araceli@pull.srl>
2024-12-06 18:16:00 -0400

    INTERFACE. se agrega el metodo para adignar la prioridad automaticamente, a la taskPriorityInterface.

--- app/Interfaces/TaskPriorityInterface.php
+++ app/Interfaces/TaskPriorityInterface.php
@@ -7,4 +7,5 @@ use App\Models\Task;
 interface TaskPriorityInterface
 {
     public function setPriority(Task $task, string $priority): Task;
+    public function autoAssignPriority(Task $task): Task;
 }
