e0d42ebe20b4f1bd78cc7208ee0587dbd0d67fe7
AraceliHT <araceli@pull.srl>
2024-12-06 17:04:56 -0400

    FORMREQ. creacion del TaskRequest con las validaciones para task

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,3 @@ Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 Route::resource('tasks', TaskController::class);
-
-// Route::view('tasks/create', 'tasks.create')->name('tasks.create');
-// Route::view('tasks/{task}/edit', 'tasks.edit')->name('tasks.edit');
