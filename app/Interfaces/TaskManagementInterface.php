a3d15549a6f7dc679ab0c2d4b58733f36922f26c
AraceliHT <araceli@pull.srl>
2024-12-09 16:20:50 -0400

    M.INTERFACE: añadir un metodo que viole el ISP.

--- app/Interfaces/TaskManagementInterface.php
+++ app/Interfaces/TaskManagementInterface.php
@@ -3,10 +3,17 @@
 namespace App\Interfaces;
 
 use App\Models\Task;
+use Illuminate\Database\Eloquent\Collection;
 
 interface TaskManagementInterface
 {
+    //Estos metodos se encargan de la GESTION de tareas:
     public function createTask(array $data): Task;
     public function updateTask(int $id, array $data): Task;
     public function deleteTask(int $id): bool;
+
+    //Si implementamos un metodo para CONSULTAS se romperia el principio ISP. 
+    public function getAllTasks(): Collection;
+    //Otro metodo que romperia el principio ISP:
+    //public function getTaskById(int $id): Task;
 }
