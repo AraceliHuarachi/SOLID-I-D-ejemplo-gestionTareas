509676ceb807cffbfdb239b92966a0fc529ac683
AraceliHT <araceli@pull.srl>
2024-12-09 16:19:06 -0400

    SERVPRO: registrar el nuevo servicio para como referencia para la interface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Interfaces\TaskManagementInterface;
 use App\Interfaces\TaskPriorityInterface;
+use App\Services\AriPriorityService;
 use App\Services\TaskManagerService;
 use App\Services\TaskPriorityService;
 use Illuminate\Support\ServiceProvider;
@@ -18,7 +19,8 @@ class AppServiceProvider extends ServiceProvider
         //vincular las interfaces con sus implementaciones para que cuando se necesite una instancia 
         //de la interface, se utilice el servicio que la implementa.
         $this->app->bind(TaskManagementInterface::class, TaskManagerService::class);
-        $this->app->bind(TaskPriorityInterface::class, TaskPriorityService::class);
+        // $this->app->bind(TaskPriorityInterface::class, TaskPriorityService::class);
+        $this->app->bind(TaskPriorityInterface::class, AriPriorityService::class); //cambio de PriorityService para demostrar el ISP
     }
 
     /**
