943b7fb2e46a8fe87ded80aede5fc93c32a33457
Araceli Torrejon <aracelihtpull@gmail.com>
2024-12-09 00:44:33 -0400

    PRISERV: Transformar deadline a carbon para el correcto manejo

--- app/services/TaskPriorityService.php
+++ app/services/TaskPriorityService.php
@@ -1,6 +1,5 @@
 <?php
 
-// app/Services/TaskPriorityManager.php
 namespace App\Services;
 
 use App\Interfaces\TaskPriorityInterface;
@@ -17,17 +16,18 @@ class TaskPriorityService implements TaskPriorityInterface
 
     public function autoAssignPriority(Task $task): Task
     {
-        $currentDate = now();
-        $deadline = $task->deadline;
+        $currentDate = now()->startOfDay(); 
+        $deadline = \Carbon\Carbon::parse($task->deadline)->startOfDay();
+        //dd($currentDate, $deadline);
 
         if ($deadline && $deadline->diffInDays($currentDate) <= 3) {
             $this->setPriority($task, 'high');
-        } elseif ($deadline && $deadline->diffInDays($currentDate) <= 7) {
+        } elseif ($deadline && $deadline->diffInDays($currentDate) >= 4 && $deadline->diffInDays($currentDate) <= 15) {
             $this->setPriority($task, 'medium');
         } else {
             $this->setPriority($task, 'low');
         }
-
+    
         return $task;
     }
 }
